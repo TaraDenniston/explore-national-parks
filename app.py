27734ecb482c69b8531a17e8e89beb372897ab35
Tara Denniston <taradenniston@gmail.com>
2023-07-04 13:02:20 -0400

    add search by state form to home page route

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-import urllib.request, json
-from flask import Flask, jsonify, request, url_for, render_template
+import urllib.request
+from flask import Flask, jsonify, redirect, render_template, request, url_for
 from flask_debugtoolbar import DebugToolbarExtension
+from forms import SearchByStateForm
 from keys import SECRET_KEY, NPS_API_KEY
 from models import db, connect_db, User, Activity, Topic
 
@@ -16,11 +17,26 @@ debug = DebugToolbarExtension(app)
 
 connect_db(app)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def display_homepage():
     """Display home page and make general requests to API to populate database"""
+    
+    ### Search form for States ###
+    statesForm = SearchByStateForm()
 
-    return render_template('index.html')
+    # Parse states JSON file into Python dict
+    # with open('static/states.json') as file:
+    #     states = json.load(file)
+    # statesList = [(item['value'], item['text']) for item in states]
+    # statesForm.state.choices = statesList
+    
+    # When states form is submitted
+    if statesForm.validate_on_submit():
+        # Get state from form data and redirect to search results
+        state = (statesForm.state.data)
+        return redirect(f'/results/states/{state}')
+
+    return render_template('index.html', statesForm=statesForm)
 
 @app.route('/activities')
 def display_activities():
