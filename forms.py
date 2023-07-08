86d82571be9d1fc5f9417f08469247fb03868105
Tara Denniston <taradenniston@gmail.com>
2023-07-08 13:14:58 -0400

    edit registration form

--- forms.py
+++ forms.py
@@ -1,12 +1,14 @@
 import json
 from flask_wtf import FlaskForm
-from wtforms import SelectField
-from wtforms.validators import InputRequired
+from wtforms import StringField, PasswordField, EmailField, SelectField, TextAreaField
+from wtforms.validators import InputRequired, Length
 
 
-
-
-
+class RegisterForm(FlaskForm):
+    email = EmailField('Email', validators=[InputRequired(), Length(max=50)])
+    password = PasswordField('Password', validators=[InputRequired()])
+    first_name = StringField('First Name', validators=[InputRequired(), Length(max=30)])
+    last_name = StringField('First Name', validators=[InputRequired(), Length(max=30)])
 
 # Parse states JSON file into Python dict
 with open('static/states.json') as file:
