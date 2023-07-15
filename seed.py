c3eb2a95a74f1ed1557e0124185a9a33b1a14c98
Tara Denniston <taradenniston@gmail.com>
2023-07-15 14:25:16 -0400

    add test user

--- seed.py
+++ seed.py
@@ -15,3 +15,13 @@ Topic.populate_table()
 Park.populate_table()
 
 # Create data for users table
+
+u1 = User(
+    email='testuser@email.com',
+    password='$2b$12$DJJLHT5BPWf5eIh4ryHIcOhOeb5.HZxGIJDI7wQsNrGPU6EVapD6u',   
+    first_name='Test',
+    last_name='User'
+)
+
+db.session.add_all([u1])
+db.session.commit()
