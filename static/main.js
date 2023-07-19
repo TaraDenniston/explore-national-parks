9ec5eb4654e9908690e4c4d90519ab29138df089
Tara Denniston <taradenniston@gmail.com>
2023-07-19 10:02:25 -0400

    add js function to toggle favorite status when star icon is clicked

--- static/main.js
+++ static/main.js
@@ -1,81 +1,59 @@
 "use strict";
 
-const $activities = $('#activities');
-// const $familyDetails = $('#family-details');
-// let currentUser;
+
+const $body = $("body");
 
 
-/**********************************************************************************
- *  Get list of activities from NPS API
- *
- *  Returns array objects that contain: 
- *  {id, activityName}
- **********************************************************************************/
-async function getActivities() {
-  console.log('inside getActivities()');
-  let activities = [];
-  
-//   // make GET request to NPS API
-//   await $.get( `${BASE_URL}/activities`, function( data ) {
+// Listen for click on all star icons
+async function toggleFavorite(evt) {
+  evt.preventDefault();
 
-  //make GET request to NPS API
-  const response = await axios({
-    url: `${BASE_URL}/activities`,
-    method: 'GET',
-    headers: {
-        'Authorization': `X-Api-Key: ${KEY}`
+  const $star = $(evt.target);
+
+  // Fetch data from the target
+  const parkCode = $star.data("park");
+  const userId = $star.data("user");
+  const url = $star.data("url");
+  let favValue = $star.data("fav");
+
+  // Set new favorite value
+  let newFavValue = !favValue;
+
+  // Only update if a user is logged in
+  if(userId) {
+    const favData = `{"park":"${parkCode}", "user":"${userId}", "favValue":"${newFavValue}"}`
+
+    // Make a post request to update favorite status
+    $.ajax({
+      type: "POST",
+      contentType: 'application/json',
+      url: url,
+      dataType: 'json',
+      data: JSON.stringify(favData)
+    });
+
+    // If park is not favorited, toggle attributes on
+    if (!favValue) {
+      $star.attr("class", "fav fa-solid fa-star");
+      $star.attr("style", "color: #f7d702;");
+      $star.data("fav", "true");
     }
-  });
 
-  console.log(`response: ${response}`);
-  let properties = Object.getOwnPropertyNames(response);
-  console.log(`typeof(response): ${typeof(response)}`)
-  console.log(`properties of response: ${properties}`);
-  
-//   console.log(data);
-//   // from the result, create an Activity object  
-//   activity = new Activity({
-//     id: data.id,
-//     name: data.name,
-//   });  
-  
-//   console.log(activities[0]);
-  
-  return activities;
+    // If park is already favorited, toggle attributes off
+    if (favValue) {
+      $star.attr("class", "fav fa-regular fa-star");
+      $star.attr("style", "color: #bababa;");
+      $star.data("fav", "false");
+    }
+  } 
 }
 
 
 /**********************************************************************************
- * Display details on DOM
- * 
- * Calls getMhUserInfo() and displays returned information about user in the user
- * details section
- **********************************************************************************/
-async function displayDetails() {
-
-  // Get list of activities from the API
-  let activities = await getActivities();
-  
-  // // Display user info on the DOM
-  // displayUser(user);
-  
-    // // Get family info from the API
-    // console.log(`Individual ID: ${user.individual}`)
-    // let family = await createFamily(user.individual);
-  
-    // let properties = Object.getOwnPropertyNames(family);
-    // console.log(`family: ${family}`)
-    // console.log(family);
-    // console.log(`typeof(family): ${typeof(family)}`)
-    // console.log(`properties of family: ${properties}`);
-  
-    // displayFamily(family);
-}
-
-
-/**********************************************************************************
- * When document has finished loading, execute the display function 
+ * When document has finished loading, execute the functions
  **********************************************************************************/
 $(function() {
-  displayDetails();
+  $('[data-toggle="tooltip"]').tooltip();
+  $('[data-toggle="popover"]').popover();
+  $body.on("click", "i.fav", toggleFavorite);
 });
