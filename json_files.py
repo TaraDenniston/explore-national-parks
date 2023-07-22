ef7001ab4aee2080d662ee785aac87586487dfaf
Tara Denniston <taradenniston@gmail.com>
2023-07-22 08:23:33 -0400

    create python file for holding methods for creating JSON files

---
+++ json_files.py
@@ -0,0 +1,36 @@
+import urllib.request
+from keys import NPS_API_KEY
+from models import BASE_URL
+
+
+def create_activities_json_file():
+    """Create JSON file with activities data collected from the NPS API"""
+
+    # Make request to API
+    url = f'{BASE_URL}/activities?api_key={NPS_API_KEY}'
+    response = urllib.request.urlopen(url)
+
+    # Decode the response
+    data = response.read().decode("UTF-8")
+
+    # Create JSON file
+    with open('static/activities.json', 'w') as outfile:
+        outfile.write(data)
+
+    return
+
+def create_topics_json_file():
+    """Create JSON file with topics data collected from the NPS API"""
+
+    # Make request to API
+    url = f'{BASE_URL}/topics?limit=100&api_key={NPS_API_KEY}'
+    response = urllib.request.urlopen(url)
+
+    # Decode the response
+    data = response.read().decode("UTF-8")
+
+    # Create JSON file
+    with open('static/topics.json', 'w') as outfile:
+        outfile.write(data)
+
+    return
