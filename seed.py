f562ae5d5b1771edfc017541579cebc2416f41eb
Tara Denniston <taradenniston@gmail.com>
2023-07-29 12:39:06 -0400

    update seed file to include favorites and notes

--- seed.py
+++ seed.py
@@ -1,4 +1,4 @@
-from models import db, User, Park, Favorite
+from models import db, User, Park, Note
 from json_files import create_activities_json_file, create_topics_json_file
 from app import app
 
@@ -15,23 +15,54 @@ create_topics_json_file()
 # Populate parks table
 Park.populate_table()
 
+
 # Create data for users table
 u1 = User(
-    email='testuser@email.com',
-    password='$2b$12$DJJLHT5BPWf5eIh4ryHIcOhOeb5.HZxGIJDI7wQsNrGPU6EVapD6u',   
-    first_name='Test',
-    last_name='User'
+    email='testuser1@email.com',
+    password='$2b$12$iFRR4FQYqSrCOiJ0ZDhoyOcOL4aPWYCktYRY2KKO6iKK5TFZ4tpPm',   
+    first_name='Test1',
+    last_name='User1'
 )
 
-db.session.add_all([u1])
+u2 = User(
+    email='testuser2@email.com',
+    password='$2b$12$KgdKBO/UDaPaXM9b23ri9.FiwqY2rOZA6HKEg2NbnLAd6Zl4E68pq',   
+    first_name='Test2',
+    last_name='User2'
+)
+
+db.session.add_all([u1, u2])
 db.session.commit()
 
 
 # Create data for favorites table
-u1f1 = Favorite(
+u1 = User.query.filter_by(email='testuser1@email.com').first()
+
+u1.favorites.append(Park.query.get('arch'))
+u1.favorites.append(Park.query.get('yose'))
+u1.favorites.append(Park.query.get('zion'))
+
+u2 = User.query.filter_by(email='testuser2@email.com').first()
+
+u2.favorites.append(Park.query.get('acad'))
+u2.favorites.append(Park.query.get('blue'))
+u2.favorites.append(Park.query.get('caco'))
+
+db.session.commit()
+
+
+# Create data for notes table
+n1 = Note(
     park_id = 'arch',
-    user_id = 1
+    user_id = u1.id,
+    text = 'Been there, done that.'
 )
 
-db.session.add_all([u1f1])
+n2 = Note(
+    park_id = 'blue',
+    user_id = u2.id,
+    text = 'I like to fish.'
+)
+
+db.session.add_all([n1, n2])
 db.session.commit()
