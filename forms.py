3a6146397325d0e99ed98f87812acb171b0cfc2c
Tara Denniston <taradenniston@gmail.com>
2023-07-29 12:38:39 -0400

    add forms for editing user details, updating password, and updating park notes

--- forms.py
+++ forms.py
@@ -26,6 +26,22 @@ class LoginForm(FlaskForm):
     email = EmailField('Email', validators=[InputRequired(), Length(max=50)])
     password = PasswordField('Password', validators=[InputRequired()])
 
+class EditUserForm(FlaskForm):
+    """Form for editing an existing user"""
+    email = EmailField('Email', validators=[InputRequired(), Length(max=50)])
+    first_name = StringField('First Name', validators=[InputRequired(), Length(max=30)])
+    last_name = StringField('First Name', validators=[InputRequired(), Length(max=30)])
+
+class EditPasswordForm(FlaskForm):
+    """Form for changing a user's pasword"""
+    curr_pw = PasswordField('Password', validators=[InputRequired()])
+    new_pw_1 = PasswordField('Password', validators=[InputRequired()])
+    new_pw_2 = PasswordField('Password', validators=[InputRequired()])
+
+class EditNotesForm(FlaskForm):
+    """Form for editing park notes for a user"""
+    text = TextAreaField('Notes')
+
 class SearchByStateForm(FlaskForm):
     """Form for searching parks by state"""
     state = SelectField('', 
